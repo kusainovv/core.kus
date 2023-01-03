42f879f6d16c795ef89a5703a1ced33c5eec7e7a
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-01-03 21:06:14 +0400

    update useCopyText hook --refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { useCopyText } from "../core/utils/useCopyText";
 
 export default function Home() {
-  const { copy } = useCopyText(true, 'test')
+  const { copy } = useCopyText(true, 'test', true)
   
   return <h1 onClick={(event) => copy('2')}>
     There is no any layouts, go at <i>core</i> folder.
