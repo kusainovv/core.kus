24c53547b5f131ba162851e32e3e656d56c8a876
kusainovratmir3@gmail.com <ratmirkusainov4622@gmail.com>
2023-01-06 16:57:52 +0400

    reselect global styles from page --refactoring --feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Global } from '@emotion/react'
 import type { AppProps } from 'next/app'
 import { LanguageProvider } from '../core/components/LanguageProvider'
-import { GlobalStyles } from './GlobalStyles'
+import { GlobalStyles } from '../src/GlobalStyles'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
